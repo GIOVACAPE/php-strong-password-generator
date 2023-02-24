721285628adddfd20b2ef6035032789e701a7078
Giovanni Capelli <giovacape01@gmail.com>
2023-02-24 11:37:03 +0100

    MILESTONE 3

--- includes/functions.php
+++ includes/functions.php
@@ -2,13 +2,12 @@
 function getPassword($password_length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randomString = '';
-
-    for ($i=0; $i < $password_length; $i++) {
+ 
+    for ($i = 0; $i < $password_length; $i++) {
         $index = rand(0, strlen($characters) - 1);
-        $randomString .= $characters[$index]; 
+        $randomString .= $characters[$index];
     }
-
+ 
     return $randomString;
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-<?php
+<?php 
 session_start();
 
-$password_lenght = $_GET['password-length'] ?? 0;
+$password_length = $_GET['password-length'] ?? 0;
 
 $display_alert = false;
 
-include __DIR__ .'/includes/functions.php'
+include __DIR__ . '/includes/functions.php';
 
+$_SESSION['final-password'] = getPassword($password_length);
 
+if($password_length){
+    header('Location: finalPassword.php');
+}
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,7 +21,7 @@ include __DIR__ .'/includes/functions.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Password generator GC</title>
+    <title>Password Generator di GC</title>
 </head>
 <body class="vh-100">
     <div class="bg-dark text-white text-center h-100 p-5">
@@ -29,13 +29,12 @@ include __DIR__ .'/includes/functions.php'
 
         <div class="m-5">
             <form action="" class="input-group" method="GET">
-                <input type="number" class="form-control" min="1" max="15" name="password-lenght" placeholder="Quanti caratteri deve avere la password?">
-                <button type="submit" class="btn btn-primary" type="button" >INVIA</button>
-                <!-- si potrebbe creare anche un bottone di RESET -->
+                <input type="number" class="form-control" min="1" max="15" name="password-length" placeholder="Quanti caratteri deve essere lunga la password?">
+                <button type="submit" class="btn btn-primary" type="button">Invia</button>
+                <!-- si potrebbe creare un bottone RESET -->
             </form>
         </div>
+        
     </div>
-
-    
 </body>
 </html>
